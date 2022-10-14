6791b81afde975a823eabefc9785fb0ffcebc109
Ricky La <rickylaa@hotmail.com>
2022-10-14 17:18:00 -0500

    yarn install

--- src/Auth/AuthAPI.js
+++ src/Auth/AuthAPI.js
@@ -18,7 +18,7 @@ export const AuthContextProvider = ({ children }) => {
   const actionCodeSettings = {
     // URL you want to redirect back to. The domain (www.example.com) for this
     // URL must be in the authorized domains list in the Firebase Console.
-    url: 'http://localhost:3000/login',
+    url: 'https://notes4u.onrender.com/login',
     // This must be true.
     handleCodeInApp: true,
   };
